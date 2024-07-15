7ea9699ca1a503a076453c3f5dd5da38be8c6534
Bruno Čale <bruno.313@hotmail.com>
2024-07-15 19:11:43 +0200

    feat: counter & api call

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,41 @@
-<script setup lang="ts"></script>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+const data = ref('')
+const isLoading = ref(false)
+
+const inc = () => {
+  count.value++
+}
+const dec = () => {
+  count.value--
+}
+
+async function fetchData() {
+  try {
+    isLoading.value = true
+    setTimeout(() => {
+      isLoading.value = false
+    }, 3000)
+    const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    const actualData = await response.json()
+    data.value = actualData
+  } catch (e) {
+    console.error('There was an error: ', e)
+  } finally {
+    isLoading.value = false
+  }
+}
+</script>
 
 <template>
-  <div>test1</div>
+  <p>{{ count }}</p>
+  <button @click="inc">Increment</button>
+  <button @click="dec">Decrement</button>
+  <div>
+    <button @click="fetchData">Fetch crypto data</button>
+    <p v-if="!isLoading">{{ data }}</p>
+    <p v-else>LOADING...</p>
+  </div>
 </template>
